29a5cd803d6128bb5a0c191f7799dfb365b3d88e
Bruna Menezes <brunaads.ti@gmail.com>
2026-01-30 22:21:47 -0300

    feat(domain): add Character entity with dataclass

---
+++ backend/src/domain/entities/character.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Character:
+    name: str
+    height: str
+    mass: str
+    hair_color: str
+    skin_color: str
+    eye_color: str
+    birth_year: str
+    gender: str
+    homeworld: str
+    url: str
+
+    @classmethod
+    def from_swapi(cls, data: dict) -> "Character":
+        return cls(
+            name=data["name"],
+            height=data["height"],
+            mass=data["mass"],
+            hair_color=data["hair_color"],
+            skin_color=data["skin_color"],
+            eye_color=data["eye_color"],
+            birth_year=data["birth_year"],
+            gender=data["gender"],
+            homeworld=data["homeworld"],
+            url=data["url"],
+        )
