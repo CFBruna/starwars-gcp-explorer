540ff729a86b7a81304110c83d9b6236c17a1f76
Bruna Menezes <brunaads.ti@gmail.com>
2026-01-30 22:22:13 -0300

    feat(domain): add Planet entity

---
+++ backend/src/domain/entities/planet.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Planet:
+    name: str
+    rotation_period: str
+    orbital_period: str
+    diameter: str
+    climate: str
+    gravity: str
+    terrain: str
+    surface_water: str
+    population: str
+    url: str
+
+    @classmethod
+    def from_swapi(cls, data: dict) -> "Planet":
+        return cls(
+            name=data["name"],
+            rotation_period=data["rotation_period"],
+            orbital_period=data["orbital_period"],
+            diameter=data["diameter"],
+            climate=data["climate"],
+            gravity=data["gravity"],
+            terrain=data["terrain"],
+            surface_water=data["surface_water"],
+            population=data["population"],
+            url=data["url"],
+        )
