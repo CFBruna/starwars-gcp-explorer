22c419a2902f64691b71fb184e29f2d47d0b258c
Bruna Menezes <brunaads.ti@gmail.com>
2026-01-30 22:22:28 -0300

    feat(domain): add Starship entity

---
+++ backend/src/domain/entities/starship.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Starship:
+    name: str
+    model: str
+    manufacturer: str
+    cost_in_credits: str
+    length: str
+    max_atmosphering_speed: str
+    crew: str
+    passengers: str
+    cargo_capacity: str
+    consumables: str
+    hyperdrive_rating: str
+    starship_class: str
+    url: str
+
+    @classmethod
+    def from_swapi(cls, data: dict) -> "Starship":
+        return cls(
+            name=data["name"],
+            model=data["model"],
+            manufacturer=data["manufacturer"],
+            cost_in_credits=data["cost_in_credits"],
+            length=data["length"],
+            max_atmosphering_speed=data["max_atmosphering_speed"],
+            crew=data["crew"],
+            passengers=data["passengers"],
+            cargo_capacity=data["cargo_capacity"],
+            consumables=data["consumables"],
+            hyperdrive_rating=data["hyperdrive_rating"],
+            starship_class=data["starship_class"],
+            url=data["url"],
+        )
