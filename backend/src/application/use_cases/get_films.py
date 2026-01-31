cdbc3c39bbcf7f5f332eb1aefea2a9fed61b1b5d
Bruna Menezes <brunaads.ti@gmail.com>
2026-01-30 22:25:42 -0300

    feat(application): implement GetFilms use case

---
+++ backend/src/application/use_cases/get_films.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from application.ports.swapi_client import SwapiClient
+from domain.entities.film import Film
+from domain.value_objects.filters import SearchFilters
+
+
+class GetFilms:
+    """Use case: Retrieve Star Wars films with optional search"""
+
+    def __init__(self, swapi_client: SwapiClient):
+        self.swapi_client = swapi_client
+
+    async def execute(self, filters: SearchFilters) -> dict:
+        """
+        Execute use case to get films from SWAPI
+
+        Returns dict with: {"count": int, "results": List[Film]}
+        """
+        response = await self.swapi_client.get_films(filters)
+        films = [Film.from_swapi(item) for item in response["results"]]
+        return {"count": response["count"], "results": films}
