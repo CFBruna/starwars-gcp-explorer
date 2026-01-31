8e435e25c836260c0ebd022f144b6d2a764541e7
Bruna Menezes <brunaads.ti@gmail.com>
2026-01-30 22:25:37 -0300

    feat(application): implement GetPlanets use case

---
+++ backend/src/application/use_cases/get_planets.py
@@ -0,0 +1,22 @@
+from typing import List
+
+from application.ports.swapi_client import SwapiClient
+from domain.entities.planet import Planet
+from domain.value_objects.filters import SearchFilters
+
+
+class GetPlanets:
+    """Use case: Retrieve Star Wars planets with optional search"""
+
+    def __init__(self, swapi_client: SwapiClient):
+        self.swapi_client = swapi_client
+
+    async def execute(self, filters: SearchFilters) -> dict:
+        """
+        Execute use case to get planets from SWAPI
+
+        Returns dict with: {"count": int, "results": List[Planet]}
+        """
+        response = await self.swapi_client.get_planets(filters)
+        planets = [Planet.from_swapi(item) for item in response["results"]]
+        return {"count": response["count"], "results": planets}
