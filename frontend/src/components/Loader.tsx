c1c355cfb3010417a5424390611f84ca1638ee6c
Bruna Menezes <brunaads.ti@gmail.com>
2026-02-01 00:04:43 -0300

    feat(frontend): add SearchBar and Loader components

---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+    return (
+        <div className="flex items-center justify-center py-12">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-yellow-500"></div>
+        </div>
+    );
+}
